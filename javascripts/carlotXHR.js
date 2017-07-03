5a70bc57af90ae8efcc50c28018c1d720972bb18
Aspen Humphrey <aspenjadedimmick@gmail.com>
2017-07-03 14:33:04 -0500

    setting up challenge with read me- 1 & 2 completed. have a javascripts folder

---
+++ javascripts/carlotXHR.js
@@ -0,0 +1,22 @@
+var CarLot = (function(globalScopeCarLot) {
+
+    // Define a private scope variable to store cars
+    let _car_inventory = [];
+
+    // Start building the Object that will be attached
+    // to the CarLot.Inventory namespace
+    let inventory = Object.create(null);
+    inventory.loadInventory = function(callback) {
+        var inventoryLoader = new XMLHttpRequest();
+        inventoryLoader.addEventListener("load", function() {
+            // Add each car to the private _car_inventory array
+          _car_inventory = JSON.parse(event.target.responseText).cars;
+            
+        });
+        inventoryLoader.open("GET", "inventory.json");
+        inventoryLoader.send();
+    }
+    globalScopeCarLot.Inventory = inventory;
+    return globalScopeCarLot
+
+}(CarLot || {}));
